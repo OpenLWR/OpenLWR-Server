4bd38fd0efcf7fb97f01b887ac06754ad792333f
Roger van Bommel <fjvanbommel@gmail.com>
2024-04-27 19:15:05 +0200

    add a proper timestep/timedelta system

--- simulation/init_simulation.py
+++ simulation/init_simulation.py
@@ -1,8 +1,9 @@
 import time
+import math
+import importlib
 from server.server_events import server_meter_parameters_update_event
 from server.server_events import server_indicator_parameters_update_event
 from server.server_events import server_alarm_parameters_update_event
-import importlib
 import config
 
 # import the model specified in the config
@@ -10,15 +11,30 @@ model = importlib.import_module(f"simulation.models.{config.model}.model")
 
 class Simulation:
     def __init__(self):
+        self.timestep = 0.1 # time between model steps
+        self.default_timestep = 0.1 # what is 1x speed
+        self.minimum_speedup_drop = 2 # skip sending packets if we exceed this many times 1x speed
+        self.timesteps = 0 # how many timesteps did we take
         self.timer()
-            
+
     def timer(self):
         # TODO: proper timer
         while True:
+            start = time.perf_counter()
             model.model_run()
-            server_meter_parameters_update_event.fire(model.values)
-            server_indicator_parameters_update_event.fire(model.indicators)
-            server_alarm_parameters_update_event.fire(model.alarms)
-            time.sleep(0.1)
+            drop = 0
+            if (self.default_timestep/self.timestep) >= self.minimum_speedup_drop:
+                drop = self.timesteps % math.floor((self.default_timestep/self.timestep)/self.minimum_speedup_drop)
+            if drop == 0: # prevent flooding clients on high speedups
+                server_meter_parameters_update_event.fire(model.values)
+                server_indicator_parameters_update_event.fire(model.indicators)
+                server_alarm_parameters_update_event.fire(model.alarms)
+            end = time.perf_counter()
+            delta = end - start
+            if self.timestep - delta < 0:
+                print(f"[ALERT] simulation cant keep up, time we have:{self.timestep}, time we took: {delta}")
+            else:
+                time.sleep(self.timestep - delta)
+            self.timesteps += 1
 
 simulation = Simulation()
