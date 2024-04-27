adad3727aa8ed122e65c38bb3e269cef1ab84b93
watchful5406 <seantyler525@gmail.com>
2024-04-26 23:52:38 -0700

    Fix module being given more arguments than needed

--- simulation/models/control_room_nmp2/model.py
+++ simulation/models/control_room_nmp2/model.py
@@ -90,5 +90,5 @@ def model_run():
     #TODO: import reactor protection system and annunciator logic, and run them here
     annunciators.run(alarms,buttons)
     reactor_protection_system.run(alarms,buttons,indicators,rods,switches)
-    rod_drive_control_system.run(rods,buttons)
-    rod_position_information_system.run(rods,alarms)
+    rod_drive_control_system.run(rods)
+    rod_position_information_system.run(rods,alarms,buttons)
