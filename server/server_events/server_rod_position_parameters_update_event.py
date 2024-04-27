95d20bc7a1b3b2640f5365453417843af90969da
Roger van Bommel <fjvanbommel@gmail.com>
2024-04-27 20:40:39 +0200

    update rod server code to only send changed rods

--- server/server_events/server_rod_position_parameters_update_event.py
+++ server/server_events/server_rod_position_parameters_update_event.py
@@ -4,9 +4,19 @@ from server.constants import packets
 import json
 import config
 import importlib
+old_rods = {}
 
 def fire(rods): #TODO only send ones that need to be updated (were changed)
-    manager.broadcast_packet(packet_helper.build(packets.ServerPackets.ROD_POSITION_PARAMETERS_UPDATE, json.dumps(rods)))
+    new_rods = json.loads(json.dumps(rods)) # hack to copy this properly
+    for rod_name,rod in rods.items():
+        if old_rods.get(rod_name) == rod:
+            del new_rods[rod_name]
+        else:
+            old_rods[rod_name] = json.loads(json.dumps(rod)) # hack to copy this properly
+            print("sent:",rod_name)
+
+    if new_rods != {}:
+        manager.broadcast_packet(packet_helper.build(packets.ServerPackets.ROD_POSITION_PARAMETERS_UPDATE, json.dumps(new_rods)))
 
 # send initial state of indicators
 def fire_initial(token):
