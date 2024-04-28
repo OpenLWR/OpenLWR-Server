3e49cac61699e0977f1f6fcb4b411f36ab53f9ac
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2024-04-27 22:08:10 -0700

    Add username

--- server/init_server.py
+++ server/init_server.py
@@ -14,8 +14,24 @@ from server.server_events import server_rod_position_parameters_update_event
 from server.client_events import client_rod_select_update_event
 
 def init_server(websocket):
-    token_str = str(uuid.uuid4())
-    token_object = manager.connect(websocket, token_str)
+    for packet in websocket:
+        try:
+            packet_id, packet_data = packet_helper.parse(packet)
+            if packet_id != packets.ClientPackets.USER_LOGIN:
+                raise Exception("User login packet invalid.")
+            username = packet_data
+            print(len(username))
+            if len(username) <= 20 and len(username) >= 2:
+                token_str = str(uuid.uuid4())
+                token_object = manager.connect(websocket, token_str)
+                manager.set_username(token_str, username)
+                manager.broadcast_packet(packet_helper.build(packets.ServerPackets.USER_LOGIN_ACK, ""))
+                break
+            else:
+                raise Exception("Username invalid.")
+        except Exception as e: 
+            print(e)
+            manager.disconnect(token_str)
 
     # inform the client of all switch positions
     server_switch_parameters_update_event.fire_initial(token_str)
