dd48cbbecf0555adc51e70e312691e0a431be540
kcinnaJlol <39594309+kcinnaJlol@users.noreply.github.com>
2024-07-05 22:38:34 +0200

    add support for getting the server's status

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from websockets.sync.server import serve
+from websockets.http11 import Response
+from websockets.datastructures import Headers
+import websockets
 import config
 from server import init_server
 import threading
+import json
 
 def main():
-    with serve(init_server.init_server, config.server_ip, 7001) as server:
+    with serve(init_server.init_server, config.server_ip, 7001,
+               process_request=process_request) as server:
         server.serve_forever()
+
+def process_request(connection, headers):
+    path = connection.request.path
+    print(path)
+    if path == "/status":
+        response = bytes(json.dumps({'status': '{}'.format(config.model), 'model': config.model, 'motd': motd()}),'UTF-8')
+        return Response(200, "OK", Headers({'Content-Type': 'application/json', 'Content-Length': len(response)}),
+                        response)
+    return None
+
+def motd():
+    return """[p align=center][font_size=48]OpenLWR[/font_size]
+version i have no fucking clue
+running model {}[/p]""".format(config.model)
 
 def import_simulation():
     import simulation
