e084bb8fa99856f9295494fca28ab462241a4c6c
watchful5406 <seantyler525@gmail.com>
2024-10-24 20:16:50 -0700

    a

--- server/server_events/server_voip_event.py
+++ server/server_events/server_voip_event.py
@@ -3,4 +3,6 @@ from server.connection_manager import manager
 from server.constants import packets
 
 def fire(data,player):
-    manager.broadcast_packet(packet_helper.build(packets.ServerPackets.VOIP, "%s|%s" % (data,player)))
+
+    for user in manager.tokens:
+        manager.send_user_packet(packet_helper.build(packets.ServerPackets.VOIP, "%s|%s" % (data,player)),manager.tokens[user].token)
