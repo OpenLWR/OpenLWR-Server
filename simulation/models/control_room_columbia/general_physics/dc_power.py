6b9da9132900141dd0a46f49684b5a65543080a7
Watchful <95109681+watchful5406@users.noreply.github.com>
2025-02-18 20:08:52 -0800

    Testing

--- simulation/models/control_room_columbia/general_physics/dc_power.py
+++ simulation/models/control_room_columbia/general_physics/dc_power.py
@@ -8,9 +8,12 @@ class Battery:
         self.voltage = voltage
         self.normal_voltage = voltage
 
+    def SetCharge(self,percent):
+        self.amphours = self.normal_amphours*(percent/100)
+
     def calculate(self):
 
-        amps = 15 #This should work for negative numbers, which is effectively charging the battery.
+        amps = 1000 #This should work for negative numbers, which is effectively charging the battery.
 
         amp_hours_used = (amps*0.1) #Delta time.
 
@@ -20,3 +23,53 @@ class Battery:
 
         self.voltage = (self.amphours/self.normal_amphours)*self.normal_voltage
 
+Batt = Battery(130,findAmpHoursToZero())
+
+i = 0
+import time
+
+import matplotlib.pyplot as plt
+
+class Transient:
+    def __init__(self,name):
+        self.graphs = {}
+        self.shown = False
+        self.name = name
+
+    def add_graph(self,name):
+        x = self.graphs.copy()
+        x[name] = []
+        y = x
+        self.graphs = y
+
+    def add(self,name,value):
+        self.graphs[name].append(value)
+
+    def generate_plot(self):
+        if self.shown: return
+        self.shown = True
+
+        fig, ax = plt.subplots()
+        ax.set_title(self.name)
+        
+        for plot_name in self.graphs:
+            plot = self.graphs[plot_name]
+
+            ax.plot(plot,label=plot_name)
+
+        ax.legend()
+            
+            
+        plt.show()
+
+t = Transient("Battery 15A draw")
+
+t.add_graph("Voltage")
+
+while i < 500:
+    i+=1
+    Batt.calculate()
+    t.add("Voltage",round(Batt.voltage,4))
+    time.sleep(0.01)
+
+t.generate_plot()
