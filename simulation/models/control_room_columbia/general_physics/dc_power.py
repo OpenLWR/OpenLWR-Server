3fce345c696e8652bc9e49e084d9c5833ab186de
watchful5406 <seantyler525@gmail.com>
2025-02-18 22:38:55 -0800

    fix discharge rate

--- simulation/models/control_room_columbia/general_physics/dc_power.py
+++ simulation/models/control_room_columbia/general_physics/dc_power.py
@@ -17,59 +17,9 @@ class Battery:
 
         amp_hours_used = (amps*0.1) #Delta time.
 
-        amp_hours_used = (amps/3600) #To hours
+        amp_hours_used = (amps/36000) #To hours
 
         self.amphours = self.amphours-amp_hours_used
 
         self.voltage = (self.amphours/self.normal_amphours)*self.normal_voltage
 
-Batt = Battery(130,findAmpHoursToZero())
-
-i = 0
-import time
-
-import matplotlib.pyplot as plt
-
-class Transient:
-    def __init__(self,name):
-        self.graphs = {}
-        self.shown = False
-        self.name = name
-
-    def add_graph(self,name):
-        x = self.graphs.copy()
-        x[name] = []
-        y = x
-        self.graphs = y
-
-    def add(self,name,value):
-        self.graphs[name].append(value)
-
-    def generate_plot(self):
-        if self.shown: return
-        self.shown = True
-
-        fig, ax = plt.subplots()
-        ax.set_title(self.name)
-        
-        for plot_name in self.graphs:
-            plot = self.graphs[plot_name]
-
-            ax.plot(plot,label=plot_name)
-
-        ax.legend()
-            
-            
-        plt.show()
-
-t = Transient("Battery 15A draw")
-
-t.add_graph("Voltage")
-
-while i < 500:
-    i+=1
-    Batt.calculate()
-    t.add("Voltage",round(Batt.voltage,4))
-    time.sleep(0.01)
-
-t.generate_plot()
