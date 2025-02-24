4a470f6941644e864ebe83f1b924c57d16521520
watchful5406 <seantyler525@gmail.com>
2025-02-23 18:49:17 -0800

    client ping-pongs during download
    this allows the client to not be overwhelmed with data and make connections more stable

--- server/init_server.py
+++ server/init_server.py
@@ -26,82 +26,96 @@ import time
 from server import rcon
 rcon_init = False
 
-
 def init_server(websocket):
     import log
     import config
     if not rcon_init:
         rcon.init()
+
+    login_step = 0
+    model_copy = None
+
     try:
         for packet in websocket:
             try:
-                token_str = str(uuid.uuid4())
-                packet_id, packet_data = packet_helper.parse(packet)
+                if login_step == 0:
+                    token_str = str(uuid.uuid4())
+                    packet_id, packet_data = packet_helper.parse(packet)
 
-                login_parameters = json.loads(packet_data)
+                    login_parameters = json.loads(packet_data)
 
-                username = login_parameters["username"]
-                version = login_parameters["version"]
+                    username = login_parameters["username"]
+                    version = login_parameters["version"]
 
-                token_object = manager.connect(websocket, token_str)
-                manager.set_username(token_str, username)
+                    # check the packet is the correct type and the username is valid
+                    assert packet_id == packets.ClientPackets.USER_LOGIN, "Client sent an invalid packet while logging in."
+                    assert (len(username) <= 20 and len(username) >= 2), "Client has an invalid username."
 
-                # check the packet is the correct type and the username is valid
-                assert packet_id == packets.ClientPackets.USER_LOGIN, "Client sent an invalid packet while logging in."
-                assert (len(username) <= 20 and len(username) >= 2), "Client has an invalid username."
+                    # check to make sure the client has the same version as the server
+                    assert version == config.config["version"], f"Version mismatch. Client has {version} - Server has {config.config["version"]}"
 
-                # check to make sure the client has the same version as the server
-                assert version == config.config["version"], f"Version mismatch. Client has {version} - Server has {config.config["version"]}"
+                    login_step += 1
 
                 #before logging in the client, we need to actually give them all the data
                 #make a copy of the current state of model.py
 
-                model = importlib.import_module(f"simulation.models.{config.config["model"]}.model")
+                    model = importlib.import_module(f"simulation.models.{config.config["model"]}.model")
 
-                recorders = {}
-                model_recorders = model.recorders.copy()
-                for recorder in model_recorders:
-                    recorders[recorder] = {
-                        "channels":model_recorders[recorder].channels,
-                        "buttons":model_recorders[recorder].buttons,
-                        "page":model_recorders[recorder].page,
-                        "elements":model_recorders[recorder].elements,
-                        "display_on":model_recorders[recorder].display_on,
+                    recorders = {}
+                    model_recorders = model.recorders.copy()
+                    for recorder in model_recorders:
+                        recorders[recorder] = {
+                            "channels":model_recorders[recorder].channels,
+                            "buttons":model_recorders[recorder].buttons,
+                            "page":model_recorders[recorder].page,
+                            "elements":model_recorders[recorder].elements,
+                            "display_on":model_recorders[recorder].display_on,
+                        }
+
+                    model_rods = copy.deepcopy(model.rods) #sanitize rods sent to the client
+                    for rod in model_rods:
+                        model_rods[rod].pop("neutrons")
+                        model_rods[rod].pop("measured_neutrons_last")
+                        model_rods[rod].pop("measured_neutrons")
+                        model_rods[rod].pop("driftto")
+                        model_rods[rod].pop("driving")
+                        model_rods[rod].pop("reed_switch_fail")
+                        model_rods[rod].pop("accum_pressure")
+
+                    model_copy = {
+                        "switches" : model.switches.copy(),
+                        #"values" : model.values.copy(), #This will stay on the client
+                        "buttons" : model.buttons.copy(),
+                        #"indicators" : model.indicators.copy(), #This will stay on the client
+                        "alarms" : model.alarms.copy(),
+                        "rods" : model_rods,
+                        "recorders" : recorders,
                     }
 
-                model_rods = copy.deepcopy(model.rods) #sanitize rods sent to the client
-                for rod in model_rods:
-                    model_rods[rod].pop("neutrons")
-                    model_rods[rod].pop("measured_neutrons_last")
-                    model_rods[rod].pop("measured_neutrons")
-                    model_rods[rod].pop("driftto")
-                    model_rods[rod].pop("driving")
-                    model_rods[rod].pop("reed_switch_fail")
-                    model_rods[rod].pop("accum_pressure")
+                if login_step > 0:
+                    if login_step <= 5:
+                        i = 1
+                        for table_name in model_copy:
+                            if i == login_step:
+                                info = model_copy[table_name]
 
-                model_copy = {
-                    "switches" : model.switches.copy(),
-                    #"values" : model.values.copy(), #This will stay on the client
-                    "buttons" : model.buttons.copy(),
-                    #"indicators" : model.indicators.copy(), #This will stay on the client
-                    "alarms" : model.alarms.copy(),
-                    "rods" : model_rods,
-                    "recorders" : recorders,
-                }
+                                data_to_send = f"{json.dumps(info)}|{json.dumps(table_name)}" #Do we actually need to json.dumps the table name?
 
-                for table_name in model_copy:
-                    info = model_copy[table_name]
+                                websocket.send(packet_helper.build(packets.ServerPackets.DOWNLOAD_DATA,data_to_send))
+                                break
+                            i+=1
+                    else:
 
-                    data_to_send = f"{json.dumps(info)}|{json.dumps(table_name)}" #Do we actually need to json.dumps the table name?
+                        #TODO: Checksum the client against the server
 
-                    manager.send_user_packet(packet_helper.build(packets.ServerPackets.DOWNLOAD_DATA,data_to_send),token_object.token)
+                        #last, we finally broadcast the login packet
+                        websocket.send(packet_helper.build(packets.ServerPackets.USER_LOGIN_ACK,"ok"))
+
+                        token_object = manager.connect(websocket, token_str)
+                        manager.set_username(token_str, username)
+                        break
+                    login_step+=1
                 
-                    time.sleep(0.05) #yeah whatever why not
-                #TODO: Checksum the client against the server
-
-                #last, we finally broadcast the login packet
-                manager.send_user_packet(packet_helper.build(packets.ServerPackets.USER_LOGIN_ACK,"ok"),token_object.token)
-                break
 
             except AssertionError as msg:
                 # login invalid
