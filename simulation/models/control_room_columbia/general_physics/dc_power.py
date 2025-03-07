329aad43a6584c00f7fbda2a1437cb4ab1e680d1
watchful5406 <seantyler525@gmail.com>
2025-03-06 16:20:56 -0800

    more DC stuff yeah

--- simulation/models/control_room_columbia/general_physics/dc_power.py
+++ simulation/models/control_room_columbia/general_physics/dc_power.py
@@ -8,6 +8,9 @@ class Battery:
         self.voltage = voltage
         self.normal_voltage = voltage
 
+    def whoami(self):
+        return self.__class__
+
     def SetCharge(self,percent):
         self.amphours = self.normal_amphours*(percent/100)
 
@@ -23,3 +26,78 @@ class Battery:
 
         self.voltage = (self.amphours/self.normal_amphours)*self.normal_voltage
 
+class Bus:
+    def __init__(self,voltage):
+        self.voltage = voltage
+        self.normal_voltage = voltage
+        self.loads = {}
+        self.batteries = []
+        self.feeders = []
+        self.current = 0
+    
+    def whoami(self):
+        return self.__class__
+
+    def AddBattery(self,battery):
+        self.batteries.append(battery)
+
+    def AddFeeder(self,feeder):
+        self.feeders.append(feeder)
+
+    def AddLoad(self,load,name):
+        if not name in self.loads:
+            self.loads[name] = load
+
+    def ModifyLoad(self,load,name):
+        if name in self.loads:
+            self.loads[name] = load
+
+    def RemoveLoad(self,name):
+        if name in self.loads:
+            self.loads.pop(name)
+
+    def calculate(self):
+        a = 0
+
+class Fuse:
+    def __init__(self,disconnect_current):
+        self.disconnect_current = disconnect_current
+        self.current = 0
+        self.running = None
+        self.incoming = None
+        self.closed = True
+
+    def whoami(self):
+        return self.__class__
+
+    def SetIncoming(self,incoming):
+        self.incoming = incoming
+
+    def SetRunning(self,running):
+        self.running = running
+
+    def Repair(self):
+        self.closed = True
+
+    def calculate(self):
+        if self.current >= self.disconnect_current:
+            self.closed = False
+
+class Breaker:
+    def __init__(self):
+        self.current = 0
+        self.running = None
+        self.incoming = None
+        self.closed = True
+
+    def whoami(self):
+        return self.__class__
+
+    def SetIncoming(self,incoming):
+        self.incoming = incoming
+
+    def SetRunning(self,running):
+        self.running = running
+
+    def calculate(self):
+        a = 0
